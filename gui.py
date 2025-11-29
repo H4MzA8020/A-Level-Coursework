b24c9271cf567cc9a51fd1ef61cfb5e1c9e14d72
Hamza <hazizkhan8002@gmail.com>
2025-11-29 20:50:29 +0000

    Working button to guide. Working guide window.

--- gui.py
+++ gui.py
@@ -1,8 +1,18 @@
 import tkinter as tk
 #Use arial font for all.
 
-def mainMenu():
-    #Parent window.
+def openGuideMenu(Parent):
+#TODO: Parent.Destroy, Parent.Open, guide.Destroy when press press back
+    Parent.destroy()
+
+    guideWindow = tk.Tk()
+    guideWindow.title("H.A.B.S Guide")
+
+    guideTitle = tk.Label(guideWindow, text = "H.A.B.S Guide", font = ("Arial", 64, "bold"))
+    guideTitle.place(relx = 0.5, rely = 0.05)
+
+def openMainMenu():
+    #Pass parent variable into the functions which are linked to buttons.
     mainWindow = tk.Tk()
     mainWindow.title("Historically Accurate Battle Simulator!")
 
@@ -13,7 +23,7 @@ def mainMenu():
     battleButton = tk.Button(mainWindow, text = "Battle", width = 20)
     battleButton.place(relx = 0.5, rely = 0.6)
 
-    guideButton = tk.Button(mainWindow, text = "Guide", width = 20)
+    guideButton = tk.Button(mainWindow, text = "Guide", width = 20, command = lambda: openGuideMenu(mainWindow) )
     guideButton.place(relx = 0.5, rely = 0.7)
 
     settingsButton = tk.Button(mainWindow, text = "Settings", width = 20)
@@ -22,4 +32,4 @@ def mainMenu():
     mainWindow.mainloop()
 
 
-mainMenu()
+openMainMenu()
